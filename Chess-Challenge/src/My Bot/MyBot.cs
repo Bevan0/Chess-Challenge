6708c9a86bd42e54b6006a040e084449b545fdf7
Bevan0 <bevanellis8@gmail.com>
2023-07-22 09:05:23 +1200

    create EvaluateMove method

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -6,34 +6,41 @@ public class MyBot : IChessBot
     // stores piece values, None is -50 to encourage capturing
     int[] pieceValues = { -50, 100, 300, 320, 500, 1000, 10000 };
 
+    public int EvaluateMove(Move move, Board board)
+    {
+        int moveValue = 0;
+
+        PieceType capturedPieceType = board.GetPiece(move.TargetSquare).PieceType;
+        moveValue += pieceValues[(int)capturedPieceType]; // if capturing a piece, adds its value
+
+        board.MakeMove(move);
+
+        if (board.SquareIsAttackedByOpponent(move.TargetSquare)) moveValue -= (pieceValues[(int)move.MovePieceType] - 100); // punish if move is unsafe
+        if (board.IsInCheckmate()) return 100000; // always play M1
+        if (board.IsInCheck()) moveValue += 200; // add bonus if move is a check
+        if (move.IsPromotion && !board.SquareIsAttackedByOpponent(move.TargetSquare)) moveValue += 1200; // add mega bonus is move is safe promotion
+
+        if (board.IsDraw()) moveValue = 0; // if move is forced draw, set value to 0
+
+        board.UndoMove(move);
+
+        return moveValue;
+    }
+
     public Move Think(Board board, Timer timer)
     {
         Random rng = new Random();
         Move[] legalMoves = board.GetLegalMoves();
         // Set to a random move incase we don't find one
         Move moveToPlay = legalMoves[rng.Next(legalMoves.Length)];
-        int moveToPlayValue = 0;
+        int moveValue = 0;
         foreach (Move move in board.GetLegalMoves())
         {
-            int moveValue = 0;
-            PieceType capturedPieceType = board.GetPiece(move.TargetSquare).PieceType;
-            moveValue += pieceValues[(int)capturedPieceType]; // if capturing a piece, adds its value
-
-            board.MakeMove(move);
-
-            if (board.SquareIsAttackedByOpponent(move.TargetSquare)) moveValue -= (pieceValues[(int)move.MovePieceType] - 100); // punish if move is unsafe
-            if (board.IsInCheckmate()) return move; // always play M1
-            if (board.IsInCheck()) moveValue += 200; // add bonus if move is a check
-            if (move.IsPromotion && !board.SquareIsAttackedByOpponent(move.TargetSquare)) moveValue += 1200; // add mega bonus is move is safe promotion
-            
-            if (board.IsDraw()) moveValue = 0; // if move is forced draw, set value to 0
-
-            board.UndoMove(move);
-
-            if (moveToPlayValue < moveValue)
+            int thisMoveValue = EvaluateMove(move, board);
+            if (thisMoveValue > moveValue)
             {
                 moveToPlay = move;
-                moveToPlayValue = moveValue;
+                moveValue = thisMoveValue;
             }
         }
         return moveToPlay;
