dc0e729b7a27bcc16e19c6bb0f944adb2b2ed220
Bevan0 <bevanellis8@gmail.com>
2023-07-22 19:18:59 +1200

    [broken] code push to show upstream bug in MovePiece function

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,6 +5,8 @@ public class MyBot : IChessBot
 {
     // stores piece values, None is -50 to encourage capturing
     int[] pieceValues = { -50, 100, 300, 320, 500, 1000, 10000 };
+    int currentDepth = 0;
+    int maximumDepth = 4;
 
     public int EvaluateMove(Move move, Board board)
     {
@@ -19,6 +21,15 @@ public class MyBot : IChessBot
         if (board.IsInCheckmate()) return 100000; // always play M1
         if (board.IsInCheck()) moveValue += 200; // add bonus if move is a check
         if (move.IsPromotion && !board.SquareIsAttackedByOpponent(move.TargetSquare)) moveValue += 1200; // add mega bonus is move is safe promotion
+        if (move.IsEnPassant) moveValue += 1000; // en passant is super valuable
+
+        foreach (Move move2 in board.GetLegalMoves())
+        {
+            if (currentDepth >= maximumDepth) break;
+            currentDepth += 1;
+            moveValue += EvaluateMove(move2, board);
+            currentDepth -= 1;
+        }
 
         if (board.IsDraw()) moveValue = 0; // if move is forced draw, set value to 0
 
@@ -36,6 +47,7 @@ public class MyBot : IChessBot
         int moveValue = 0;
         foreach (Move move in board.GetLegalMoves())
         {
+            currentDepth = 1;
             int thisMoveValue = EvaluateMove(move, board);
             if (thisMoveValue > moveValue)
             {
